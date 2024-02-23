996d89ac3121b8b09dfd4196f901a880fa6c344b
Sanskar Rathore <sanskarrathore1553+github@gmail.com>
2024-02-23 09:11:25 +0530

    22nd feb

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -6,17 +6,28 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
-    [SerializeField] private GameObject sphere;
+   
+    [SerializeField] private GameObject lane1Cube;
+    [SerializeField] private GameObject lane2Cube;
+    [SerializeField] private GameObject lane3Cube;
+    private Vector3[] lanePos;
+        int currentLaneIndex=1;
+    int targetIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        lanePos = new Vector3[3]; //why?
+        lanePos[0] = lane1Cube.transform.position; //taking the 
+        lanePos[1] = lane2Cube.transform.position;
+        lanePos[2] = lane3Cube.transform.position;
+        Debug.Log(currentLaneIndex);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+      /*  if (Input.GetKey(KeyCode.W))
         {
             rb.AddForce(Vector3.forward*3f);
         }
@@ -27,24 +38,48 @@ public class NewBehaviourScript : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             rb.AddForce(Vector3.back * 3f);
-        }
-        if (Input.GetKey(KeyCode.A))
+        }*/
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            rb.AddForce(Vector3.left *3f);
+            if (currentLaneIndex != 0)
+                 targetIndex = currentLaneIndex-1;
+            currentLaneIndex = targetIndex;
+            Debug.Log(lanePos[currentLaneIndex]);
+            //rb.AddForce(Vector3.left *3f);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            rb.AddForce(Vector3.right * 3f);
+            if (currentLaneIndex != 2)
+                targetIndex = currentLaneIndex + 1;
+            currentLaneIndex = targetIndex;
+            Debug.Log(lanePos[currentLaneIndex]);
+            //rb.AddForce(Vector3.right * 3f);
         }
 
-
+        var step = 10f * Time.deltaTime; 
+        transform.position = Vector3.MoveTowards(transform.position, lanePos[targetIndex],step);
        
     }
     private void OnTriggerEnter(Collider other)
     {
+        
         if (other.gameObject.CompareTag("coin"))
         {
-            Destroy(sphere);
+            if (currentLaneIndex == 0)
+            {
+
+            Destroy(lane1Cube);
+            }
+            if (currentLaneIndex == 1)
+            {
+
+                Destroy(lane2Cube);
+            }
+            if (currentLaneIndex == 2)
+            {
+
+                Destroy(lane3Cube);
+            }
         }
     }
 
